a0bebf7173cd4aaf8440af249d83e3a26734b8a0
ishikapandeyy <ishikapandey.workmail@gmail.com>
2026-09-22 13:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let btn=document.querySelector("#incrementBtn")
+let count=document.querySelector("#counter")
+btn.addEventListener("click",()=>{
+	let num=Number(count.textContent)
+	alert(num)
+	count.textContent=num+1
+})
